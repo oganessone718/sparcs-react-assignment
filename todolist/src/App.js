1768b16f6008161a0416fa7f716c25fac8a74853
oganessone <oganessone718@gmail.com>
2023-04-09 22:26:26 +0900

    Title

---
+++ todolist/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <h1 id="title">To-do list</h1>
+      <hr></hr>
+    </>
+
+  );
+};
+
+export default App;
